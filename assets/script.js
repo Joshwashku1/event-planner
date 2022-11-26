a66c9978f7abd6196b4b59aa3d894075879644f8
JoshuaHamann <joshwashku@yahoo.com>
2022-11-25 21:00:51 -0600

    added divs and textareas for the user to write an event

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
 var timeBlockContainer = $('.container');
 var timeElement = $('#currentDay');
+var saveButton = $('.submit');
+
 console.log(dayjs());
+
 // handle displaying the time
 function displayTime() {
     var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeElement.text(rightNow);
 }
+
+saveButton.on('click', function() {
+    console.log(this);
+})
+
 displayTime();
 setInterval(displayTime,1000);
