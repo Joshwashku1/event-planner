b2614204486992796d3799f88c9c0dc43b012044
JoshuaHamann <joshwashku@yahoo.com>
2022-11-27 13:21:21 -0600

    added a div for the hour and assigned it to the css class

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
 var timeBlockContainer = $('.container');
 var timeElement = $('#currentDay');
-var saveButton = $('.submit');
+var saveButton = $('.save');
 
 console.log(dayjs());
 
 // handle displaying the time
 function displayTime() {
-    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    var rightNow = dayjs().format('MMM DD, YYYY  hh:mm:ss a');
     timeElement.text(rightNow);
 }
 
-saveButton.on('click', function() {
-    console.log(this);
+saveButton.on('click', function(event) {
+    var element = event.target;
+    console.log(element.parentNode.dataset.hour);
 })
 
 displayTime();
